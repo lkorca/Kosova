94ea102ae24685920cc7e232a1d03dc95700e013
lkorca <lkorca@citycollege.sheffield.eu>
2019-11-07 14:15:56 +0200

    uploaded Code.java new file

--- Code.java
+++ Code.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 public class Code{
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Kosova Republike!")
+		JOptionPane.showMessageDialog(null, "Albin Kurti!")
 	}
 }
